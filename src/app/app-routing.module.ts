e1cadc2ec55b99f00dad494c062781e877c0acd9
naresh <nasharma@bestpeers.com>
2019-07-01 19:21:08 +0530

    demo comment

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,30 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import { HomeComponent } from './home/home.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { LoginComponent } from './login/login.component';
+import { ProductDetailComponent } from './product-detail/product-detail.component';
+import { SellerHomeComponent } from './seller-home/seller-home.component';
+import { SellerLoginComponent } from './seller-login/seller-login.component';
+import { SellerRegistrationComponent } from './seller-registration/seller-registration.component';
+import { ShopComponent } from './shop/shop.component';
+import { AboutUsComponent } from './about-us/about-us.component';
+import { ContactUsComponent } from './contact-us/contact-us.component';
+import { CartComponent } from './cart/cart.component';
+import { RegistrationComponent } from './registration/registration.component';
+
+
+const routes: Routes = [
+  {path: '', redirectTo: '/home', pathMatch: 'full'},
+  {path: 'home', component: HomeComponent},
+  {path: 'login', component: LoginComponent},
+  {path: 'registration', component: RegistrationComponent},
+  {path: 'loginseller', component: SellerLoginComponent},
+  {path: 'homeseller', component: SellerHomeComponent},
+  {path: 'registrationseller', component: SellerRegistrationComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,36 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { SellerHomeComponent } from './seller-home/seller-home.component';
+import { ShopComponent } from './shop/shop.component';
+import { LoginComponent } from './login/login.component';
+import { SellerLoginComponent } from './seller-login/seller-login.component';
+import { RegistrationComponent } from './registration/registration.component';
+import { SellerRegistrationComponent } from './seller-registration/seller-registration.component';
+import { CartComponent } from './cart/cart.component';
+import { ProductDetailComponent } from './product-detail/product-detail.component';
+import { AboutUsComponent } from './about-us/about-us.component';
+import { ContactUsComponent } from './contact-us/contact-us.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    HeaderComponent,
+    FooterComponent,
+    SellerHomeComponent,
+    ShopComponent,
+    LoginComponent,
+    SellerLoginComponent,
+    RegistrationComponent,
+    SellerRegistrationComponent,
+    CartComponent,
+    ProductDetailComponent,
+    AboutUsComponent,
+    ContactUsComponent
   ],
   imports: [
     BrowserModule,
